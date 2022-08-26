a8a6b676eea24ec41890d212730241810cb3f61f
Wesley Almeida <wesley.almeida@fotosensores.com>
2022-08-26 09:36:55 -0300

    create user overridden

--- main/serializers.py
+++ main/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from .models import User
@@ -22,3 +23,17 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True},
         }
+
+    def create(self, validated_data):
+        del validated_data['confirm_password']
+        return super().create(validated_data)
+
+    def validate(self, attrs):
+        password = attrs['password']
+        confirm_password = attrs['confirm_password']
+
+        if password != confirm_password:
+            raise serializers.ValidationError('Passwords do not match')
+
+        attrs['password'] = make_password(password)
+        return attrs
