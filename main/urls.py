81e2b193e7c132ade9de04c44e056246b53f4855
Wesley Almeida <wesley.almeida@fotosensores.com>
2022-08-30 17:02:22 -0300

    AuthViewSet added

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import UserViewSet, AuthViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/login', AuthViewSet.as_view()),
 ]
