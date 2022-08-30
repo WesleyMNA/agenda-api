6bcfdd2ff7b89836a6d2da28d2106698c2f4bcd9
Wesley Almeida <wesley.almeida@fotosensores.com>
2022-08-30 17:23:24 -0300

    Auth added to tests

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,7 @@
 from json import loads
 
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
 from .models import User
@@ -9,7 +10,9 @@ from .models import User
 class UserTests(APITestCase):
 
     def setUp(self) -> None:
-        User.objects.create_user('user@email.com', 'user', 'User', 'M', '12911223344', 'password')
+        self.user = User.objects.create_user('user@email.com', 'user', 'User', 'M', '12911223344', 'password')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
 
     def test_list_users(self):
         response = self.client.get('/api/users/', format='json')
