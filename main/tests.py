4442b51a74bd190b14a1267b29bfca4ad8c23b23
Wesley Almeida <wesley.almeida@fotosensores.com>
2022-08-30 15:17:32 -0300

    test_list_users added

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from json import loads
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import User
+
+
+class UserTests(APITestCase):
+
+    def setUp(self) -> None:
+        User.objects.create_user('user@email.com', 'user', 'User', 'M', '12911223344', 'password')
+
+    def test_list_users(self):
+        response = self.client.get('/api/users/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        json = loads(response.content)
+        self.assertEqual(1, len(json))
+        user = json[0]
+        self.assertEqual('User', user['name'])
+        self.assertEqual('user', user['username'])
